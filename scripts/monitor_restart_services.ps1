1521e2a007c9fce42db501a7fcd5dfa1897d1e12
Oleksandr Maniukhin <om7755@g.rit.edu>
2024-10-29 20:54:35 -0400

    seems ready to deploy

--- scripts/monitor_restart_services.ps1
+++ scripts/monitor_restart_services.ps1
@@ -7,6 +7,6 @@ foreach ($service in $services) {
     $serviceStatus = Get-Service -Name $service
     if ($serviceStatus.Status -ne 'Running') {
         Start-Service -Name $service
-        Write-Output "$service restarted at $(Get-Date)" | Out-File "C:\ServiceLogs\RestartLog.txt" -Append
+        Write-Output "$service restarted at $(Get-Date)" | Out-File "C:\ProgramData\Steam\Logs\RestartLog.tx" -Append
     }
 }
