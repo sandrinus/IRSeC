9f4a2b6b88e91b0a4eeea458244ce7fbbc206dfc
Oleksandr Maniukhin <om7755@g.rit.edu>
2024-11-01 21:51:41 -0400

    Changed pathe s to file

--- scripts/userAdd.ps1
+++ scripts/userAdd.ps1
@@ -33,11 +33,11 @@ Import-Module ActiveDirectory  # Loads the Active Directory module to enable AD 
 # ENCRYPTED_PASSWORD_3
 
 # Specify file paths for user lists and password management
-$userListPath = "C:\ProgramData\Epic Games\IRSeC-test\username.txt"           # File containing regular usernames, one per line
-$adminUserListPath = "C:\ProgramData\Epic Games\IRSeC-test\admin_users.txt"    # File containing admin usernames, one per line
-$allUsersPath = "C:\ProgramData\Epic Games\IRSeC-test\all_users.txt"           # File containing all users to be kept in the system
-$passwordFilePath = "C:\ProgramData\Epic Games\IRSeC-test\password.txt"        # File containing encrypted passwords, one per line
-$passwordInUsePath = "C:\ProgramData\Epic Games\IRSeC-test\password_in_use.txt" # File to log the currently used password
+$userListPath = "C:\ProgramData\Epic Games\IRSeC-test\passwd\username.txt"           # File containing regular usernames, one per line
+$adminUserListPath = "C:\ProgramData\Epic Games\IRSeC-test\passwd\admin_users.txt"    # File containing admin usernames, one per line
+$allUsersPath = "C:\ProgramData\Epic Games\IRSeC-test\passwd\all_users.txt"           # File containing all users to be kept in the system
+$passwordFilePath = "C:\ProgramData\Epic Games\IRSeC-test\passwd\password.txt"        # File containing encrypted passwords, one per line
+$passwordInUsePath = "C:\ProgramData\Epic Games\IRSeC-test\passwd\password_in_use.txt" # File to log the currently used password
 
 # Hardcoded AES key for decryption (base64 encoded key provided by the user)
 $SecretKey = [Convert]::FromBase64String("deqKCoV9HjSudP1nzF0KJg==")
